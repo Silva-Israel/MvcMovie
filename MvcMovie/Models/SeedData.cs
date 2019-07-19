a89f45ba0091a5eb90fd962726f0534c3ed021c9
izzysilv <35540604+izzysilv@users.noreply.github.com>
2019-07-18 23:45:07 -0600

    Add project files.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Other Side Of Heaven",
+                        ReleaseDate = DateTime.Parse("2003-2-12"),
+                        Genre = "Romance",
+                        Rating = "PG",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Singles Ward",
+                        ReleaseDate = DateTime.Parse("2002-3-13"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 6.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("2004-2-23"),
+                        Genre = "Religious",
+                        Rating = "PG",
+                        Price = 5.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Charly",
+                        ReleaseDate = DateTime.Parse("2005-4-15"),
+                        Genre = "Romance",
+                        Rating = "PG",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
